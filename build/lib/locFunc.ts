f4da505c7ee060cfb49382d67b1fbf08e2316e54
Alex Ma <alma1@microsoft.com>
2021-04-23 11:14:49 -0700

    added changes to locFunc (extensions XLF creation)

--- build/lib/locFunc.ts
+++ build/lib/locFunc.ts
@@ -5,6 +5,8 @@
 
 import * as path from 'path';
 import * as fs from 'fs';
+import * as Is from 'is';
+import * as gulp from 'gulp';
 
 import { through, ThroughStream } from 'event-stream';
 import * as File from 'vinyl';
@@ -146,5 +148,121 @@ export function modifyI18nPackFiles(existingTranslationFolder: string, externalE
 			.catch((reason) => {
 				this.emit('error', reason);
 			});
+	});
+}
+
+interface LocalizeInfo {
+	key: string;
+	comment: string[];
+}
+
+module LocalizeInfo {
+	export function is(value: any): value is LocalizeInfo {
+		let candidate = value as LocalizeInfo;
+		return Is.defined(candidate) && Is.string(candidate.key) && (Is.undef(candidate.comment) || (Is.array(candidate.comment) && candidate.comment.every(element => Is.string(element))));
+	}
+}
+
+interface PackageJsonFormat {
+	[key: string]: string | ValueFormat;
+}
+
+interface ValueFormat {
+	message: string;
+	comment: string[];
+}
+
+interface BundledExtensionFormat {
+	[key: string]: {
+		messages: string[];
+		keys: (string | LocalizeInfo)[];
+	};
+}
+
+module PackageJsonFormat {
+	export function is(value: any): value is PackageJsonFormat {
+		if (Is.undef(value) || !Is.object(value)) {
+			return false;
+		}
+		return Object.keys(value).every(key => {
+			let element = value[key];
+			return Is.string(element) || (Is.object(element) && Is.defined(element.message) && Is.defined(element.comment));
+		});
+	}
+}
+
+export function createXlfFilesForExtensions(): ThroughStream {
+	let counter: number = 0;
+	let folderStreamEnded: boolean = false;
+	let folderStreamEndEmitted: boolean = false;
+	return through(function (this: ThroughStream, extensionFolder: File) {
+		const folderStream = this;
+		const stat = fs.statSync(extensionFolder.path);
+		if (!stat.isDirectory()) {
+			return;
+		}
+		let extensionName = path.basename(extensionFolder.path);
+		if (extensionName === 'node_modules') {
+			return;
+		}
+		counter++;
+		let _xlf: i18n.XLF;
+		function getXlf() {
+			if (!_xlf) {
+				_xlf = new i18n.XLF(extensionsProject);
+			}
+			return _xlf;
+		}
+		gulp.src([`.build/extensions/${extensionName}/package.nls.json`, `.build/extensions/${extensionName}/**/nls.metadata.json`], { allowEmpty: true }).pipe(through(function (file: File) {
+			if (file.isBuffer()) {
+				const buffer: Buffer = file.contents as Buffer;
+				const basename = path.basename(file.path);
+				if (basename === 'package.nls.json') {
+					const json: PackageJsonFormat = JSON.parse(buffer.toString('utf8'));
+					const keys = Object.keys(json);
+					const messages = keys.map((key) => {
+						const value = json[key];
+						if (Is.string(value)) {
+							return value;
+						} else if (value) {
+							return value.message;
+						} else {
+							return `Unknown message for key: ${key}`;
+						}
+					});
+					getXlf().addFile(`extensions/${extensionName}/package`, keys, messages);
+				} else if (basename === 'nls.metadata.json') {
+					const json: BundledExtensionFormat = JSON.parse(buffer.toString('utf8'));
+					const relPath = path.relative(`.build/extensions/${extensionName}`, path.dirname(file.path));
+					for (let file in json) {
+						const fileContent = json[file];
+						getXlf().addFile(`extensions/${extensionName}/${relPath}/${file}`, fileContent.keys, fileContent.messages);
+					}
+				} else {
+					this.emit('error', new Error(`${file.path} is not a valid extension nls file`));
+					return;
+				}
+			}
+		}, function () {
+			if (_xlf) {
+				let xlfFile = new File({
+					path: path.join(extensionsProject, extensionName + '.xlf'),
+					contents: Buffer.from(_xlf.toString(), 'utf8')
+				});
+				folderStream.queue(xlfFile);
+			}
+			this.queue(null);
+			counter--;
+			if (counter === 0 && folderStreamEnded && !folderStreamEndEmitted) {
+				folderStreamEndEmitted = true;
+				folderStream.queue(null);
+			}
+		}));
+	}, function () {
+		folderStreamEnded = true;
+		if (counter === 0) {
+			folderStreamEndEmitted = true;
+			this.queue(null);
+		}
 	});
 }
