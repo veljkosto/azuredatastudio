a2b23e732062aea56308f4319afbcfb792f7cda0
chgagnon <chgagnon@microsoft.com>
2021-05-11 09:01:54 -0700

    Update scripts

--- scripts/test-integration-arc.sh
+++ scripts/test-integration-arc.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
-# Runs Extension Tests
 set -e
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
 	ROOT=$(dirname $(dirname $(realpath "$0")))
-	VSCODEUSERDATADIR=`mktemp -d -t 'myuserdatadir'`
-	VSCODEEXTDIR=`mktemp -d -t 'myextdir'`
 else
 	ROOT=$(dirname $(dirname $(readlink -f $0)))
-	VSCODEUSERDATADIR=`mktemp -d 2>/dev/null`
-	VSCODEEXTDIR=`mktemp -d 2>/dev/null`
-	LINUX_NO_SANDBOX="--no-sandbox" # Electron 6 introduces a chrome-sandbox that requires root to run. This can fail. Disable sandbox via --no-sandbox.
+	# Electron 6 introduces a chrome-sandbox that requires root to run. This can fail. Disable sandbox via --no-sandbox.
+	LINUX_EXTRA_ARGS="--no-sandbox"
 fi
+
+VSCODEUSERDATADIR=`mktemp -d 2>/dev/null`
+VSCODECRASHDIR=$ROOT/.build/crashes
+cd $ROOT
 
 # Default to only running stable tests if test grep isn't set
 if [[ "$ADS_TEST_GREP" == "" ]]; then
@@ -28,21 +27,38 @@ then
 	# Run out of sources: no need to compile as code.sh takes care of it
 	INTEGRATION_TEST_ELECTRON_PATH="./scripts/code.sh"
 
+	echo "Storing crash reports into '$VSCODECRASHDIR'."
 	echo "Running integration tests out of sources."
 else
-	# Run from a built: need to compile all test extensions
+	# Configuration for more verbose output
+	export VSCODE_CLI=1
+	export ELECTRON_ENABLE_STACK_DUMPING=1
+	export ELECTRON_ENABLE_LOGGING=1
 
+	# Production builds are run on docker containers where size of /dev/shm partition < 64MB which causes OOM failure
+	# for chromium compositor that uses the partition for shared memory
+	if [ "$LINUX_EXTRA_ARGS" ]
+	then
+		LINUX_EXTRA_ARGS="$LINUX_EXTRA_ARGS  --disable-dev-shm-usage --use-gl=swiftshader"
+	fi
+
+	echo "Storing crash reports into '$VSCODECRASHDIR'."
 	echo "Running integration tests with '$INTEGRATION_TEST_ELECTRON_PATH' as build."
 fi
 
-cd $ROOT
-echo $VSCODEUSERDATADIR
-echo $VSCODEEXTDIR
+if [ -z "$INTEGRATION_TEST_APP_NAME" ]; then
+	after_suite() { true; }
+else
+	after_suite() { killall $INTEGRATION_TEST_APP_NAME || true; }
+fi
+
+ALL_PLATFORMS_API_TESTS_EXTRA_ARGS="--disable-telemetry --crash-reporter-directory=$VSCODECRASHDIR --no-cached-data --disable-updates --disable-keytar --disable-extensions --user-data-dir=$VSCODEUSERDATADIR"
 
 echo **************************
 echo *** starting arc tests ***
 echo **************************
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_NO_SANDBOX --extensionDevelopmentPath=$ROOT/extensions/arc --extensionTestsPath=$ROOT/extensions/arc/out/integrationTests --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --disable-telemetry --disable-crash-reporter --disable-updates --nogpu
+"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS --extensionDevelopmentPath=$ROOT/extensions/arc --extensionTestsPath=$ROOT/extensions/arc/out/integrationTests $ALL_PLATFORMS_API_TESTS_EXTRA_ARGS
+after_suite
 
 if [[ "$NO_CLEANUP" == "" ]]; then
 	rm -r $VSCODEUSERDATADIR
