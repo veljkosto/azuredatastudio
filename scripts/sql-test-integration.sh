2c756449f10e051ee81b7ab2fb59d450c44555f1
chgagnon <chgagnon@microsoft.com>
2022-03-18 12:05:10 -0700

    cleanup

--- scripts/sql-test-integration.sh
+++ scripts/sql-test-integration.sh
@@ -10,8 +10,16 @@ else
 	ROOT=$(dirname $(dirname $(readlink -f $0)))
 	VSCODEUSERDATADIR=`mktemp -d 2>/dev/null`
 	VSCODEEXTDIR=`mktemp -d 2>/dev/null`
-	LINUX_NO_SANDBOX="--no-sandbox" # Electron 6 introduces a chrome-sandbox that requires root to run. This can fail. Disable sandbox via --no-sandbox.
+	# {{SQL CARBON EDIT}} Completed disable sandboxing via --no-sandbox since we still see failures on our test runs
+	# --disable-setuid-sandbox: setuid sandboxes requires root and is used in containers so we disable this
+	# --disable-dev-shm-usage --use-gl=swiftshader: when run on docker containers where size of /dev/shm
+	# partition < 64MB which causes OOM failure for chromium compositor that uses the partition for shared memory
+	LINUX_EXTRA_ARGS="--no-sandbox --disable-dev-shm-usage --use-gl=swiftshader"
 fi
+
+VSCODECRASHDIR=$ROOT/.build/crashes
+VSCODELOGSDIR=$ROOT/.build/logs/sql-integration-tests
+cd $ROOT
 
 # Default to only running stable tests if test grep isn't set
 if [[ "$ADS_TEST_GREP" == "" ]]; then
@@ -26,16 +34,26 @@ then
 	# Run out of sources: no need to compile as code.sh takes care of it
 	INTEGRATION_TEST_ELECTRON_PATH="./scripts/code.sh"
 
+	echo "Storing crash reports into '$VSCODECRASHDIR'."
+	echo "Storing log files into '$VSCODELOGSDIR'."
 	echo "Running integration tests out of sources."
 else
 	# Run from a built: need to compile all test extensions
+	# because we run extension tests from their source folders
+	# and the build bundles extensions into .build webpacked
+	# {{SQL CARBON EDIT}} Don't compile unused extensions
+	yarn gulp 	compile-extension:integration-test
 
+
+	# Configuration for more verbose output
+	export VSCODE_CLI=1
+	export ELECTRON_ENABLE_STACK_DUMPING=1
+	export ELECTRON_ENABLE_LOGGING=1
+
+	echo "Storing crash reports into '$VSCODECRASHDIR'."
+	echo "Storing log files into '$VSCODELOGSDIR'."
 	echo "Running integration tests with '$INTEGRATION_TEST_ELECTRON_PATH' as build."
 fi
-
-cd $ROOT
-echo VSCODEUSERDATADIR=$VSCODEUSERDATADIR
-echo VSCODEEXTDIR=$VSCODEEXTDIR
 
 if [[ "$SKIP_PYTHON_INSTALL_TEST" == "1" ]]; then
 	echo Skipping Python installation tests.
@@ -46,24 +64,11 @@ else
 	"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_NO_SANDBOX --nogpu --extensionDevelopmentPath=$ROOT/extensions/notebook --extensionTestsPath=$ROOT/extensions/notebook/out/integrationTest --user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR --remote-debugging-port=9222 --disable-telemetry --disable-crash-reporter --disable-updates --skip-getting-started --disable-inspect
 fi
 
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_NO_SANDBOX --nogpu --extensionDevelopmentPath=$ROOT/extensions/admin-pack \
---extensionDevelopmentPath=$ROOT/extensions/admin-tool-ext-win \
---extensionDevelopmentPath=$ROOT/extensions/agent \
---extensionDevelopmentPath=$ROOT/extensions/azurecore \
---extensionDevelopmentPath=$ROOT/extensions/big-data-cluster \
---extensionDevelopmentPath=$ROOT/extensions/cms \
---extensionDevelopmentPath=$ROOT/extensions/dacpac \
---extensionDevelopmentPath=$ROOT/extensions/import \
+"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_NO_SANDBOX \
 --extensionDevelopmentPath=$ROOT/extensions/integration-tests \
---extensionDevelopmentPath=$ROOT/extensions/mssql \
---extensionDevelopmentPath=$ROOT/extensions/notebook \
---extensionDevelopmentPath=$ROOT/extensions/profiler \
---extensionDevelopmentPath=$ROOT/extensions/resource-deployment \
---extensionDevelopmentPath=$ROOT/extensions/schema-compare \
---extensionTestsPath=$ROOT/extensions/integration-tests/out/tests \
---user-data-dir=$VSCODEUSERDATADIR --extensions-dir=$VSCODEEXTDIR \
---disable-telemetry --disable-crash-reporter --disable-updates --skip-getting-started --disable-inspect
-
+--extensionTestsPath=$ROOT/extensions/integration-tests/out/test \
+--user-data-dir=$VSCODEUSERDATADIR --logsPath=$VSCODELOGSDIR --extensions-dir=$VSCODEEXTDIR \
+--nogpu --disable-telemetry --disable-crash-reporter --disable-updates --skip-welcome --disable-inspect --disable-workspace-trust --no-cached-data --crash-reporter-directory=$VSCODECRASHDIR
 
 rm -r -f $VSCODEUSERDATADIR
 rm -r $VSCODEEXTDIR
